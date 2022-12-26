fedcb5b6881fa2a1e2e2959dedb59eaf23b6cc11
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-26 22:20:28 +0000

    update refs

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-
-const formEll: document.querySelector(`#search-box`);
-const listCountries: document.querySelector(`.country-list`);
-const countryInfo: document.querySelector(`.country-info`);
+const formEll = document.querySelector(`#search-box`);
+const listCountries = document.querySelector(`.country-list`);
+const countryInfo = document.querySelector(`.country-info`);
 
 formEll.addEventListener(`input`, debounce(onFormInput, DEBOUNCE_DELAY));
 
-function onFormInput() {
+function onFormInput(e) {
   const formValue = formEll.value;
   fetchCountries(formValue).then(array => console.log(array));
 }
